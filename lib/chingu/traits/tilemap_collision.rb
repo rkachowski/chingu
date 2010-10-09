8a809eb247583959de3e1d0393c872271e9d72ea
r.kachowski <rk@toastymofo.net>
2010-10-09 15:24:15 +0100

    detection seems to be fine

--- lib/chingu/traits/tilemap_collision.rb
+++ lib/chingu/traits/tilemap_collision.rb
@@ -12,7 +12,10 @@ module Chingu
     end
     
     def update_trait
-      vertical_map_collision if self.tile_map
+      if self.tile_map
+        vertical_map_collision
+        horizontal_map_collision 
+      end
     end
       
     private
@@ -20,17 +23,15 @@ module Chingu
     #
     # checking for collisions on the left and right sides of an aabb
     #    
-    def vertical_map_collision
-      
+    def vertical_map_collision      
       #how many tiles can the top of the bounding box intersect
       no_of_checks = ( bb.w / self.tile_map.tile_width).ceil       
       no_of_checks +=1 if ((self.x-bb.w/2) % self.tile_map.tile_width) != 0
       
-      #what column in the tile_map is the left most extent of the bounding box found in
-      first_tile_column = ((self.x-bb.w/2) / self.tile_map.tile_width).floor
-
       #check top of bb
-      first_tile_row = ((self.y-bb.h/2) / self.tile_map.tile_height).floor      
+      first_tile_column = ((self.x-bb.w/2) / self.tile_map.tile_width).floor
+      first_tile_row = ((self.y-bb.h/2) / self.tile_map.tile_height).floor  
+          
       no_of_checks.times do |i|
         if self.tile_map.get_tile(first_tile_column+i,first_tile_row)
           puts "top collision!" if self.tile_map.get_tile(first_tile_column+i,first_tile_row).solid          
@@ -39,17 +40,41 @@ module Chingu
       
       #check bottom of bb      
       first_tile_row = (self.y+bb.h/2) / self.tile_map.tile_height
+      
       no_of_checks.times do |i|
         if self.tile_map.get_tile(first_tile_column+i,first_tile_row)
-          puts "check"
           puts "bottom collision!" if self.tile_map.get_tile(first_tile_column+i,first_tile_row).solid          
         end
       end
       
-      puts "beep"      
     end
     
+    #
+    # checking for collisions on left and right side of aabb
+    #
     def horizontal_map_collision
+      #how many tiles can the left of the bounding box intersect
+      no_of_checks = ( bb.h / self.tile_map.tile_height).ceil       
+      no_of_checks +=1 if ((self.y-bb.h/2) % self.tile_map.tile_height) != 0
+      
+      #check left of bb
+      first_tile_row = ((self.y-bb.h/2) / self.tile_map.tile_height).floor 
+      first_tile_column = ((self.x-bb.w/2) / self.tile_map.tile_width).floor
+           
+      no_of_checks.times do |i|
+        if self.tile_map.get_tile(first_tile_column,first_tile_row+i)
+          puts "left collision!" if self.tile_map.get_tile(first_tile_column,first_tile_row+i).solid          
+        end
+      end
+      
+      #check right of bb      
+      first_tile_column = ((self.x+bb.w/2) / self.tile_map.tile_width).floor
+      
+      no_of_checks.times do |i|
+        if self.tile_map.get_tile(first_tile_column,first_tile_row+i)
+          puts "right collision!" if self.tile_map.get_tile(first_tile_column,first_tile_row+i).solid            
+        end
+      end
       
     end
     
