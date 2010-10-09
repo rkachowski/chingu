b7313e0c879e58ebf7891afeca6129566b5cbd64
r.kachowski <rk@toastymofo.net>
2010-10-09 16:28:33 +0100

    tidied

--- lib/chingu/traits/tilemap_collision.rb
+++ lib/chingu/traits/tilemap_collision.rb
@@ -5,6 +5,8 @@
 
 module Chingu
   module TilemapCollision
+    attr_reader :b_collision,:t_collision,:l_collision,:r_collision
+    
     module ClassMethods
       def initialize_trait(options = {})
         trait_options[:tilemap_collision] = {:apply => true}.merge(options)
@@ -12,6 +14,7 @@ module Chingu
     end
     
     def update_trait
+      reset_collision_vars
       if self.tile_map
         vertical_map_collision
         horizontal_map_collision 
@@ -19,63 +22,60 @@ module Chingu
     end
       
     private
+    
+    def reset_collision_vars
+      @b_collision = false
+      @t_collision = false
+      @l_collision = false      
+      @r_collision = false      
+    end
+    
+    def vertical_map_collision       
+      @t_collision = true if collision_ver(bb.l,bb.t,bb.w)
+      @b_collision = true if collision_ver(bb.l,bb.b,bb.w)
+    end
+    
+    def horizontal_map_collision
+      @l_collision = true if collision_hor(bb.l,bb.t,bb.h)
+      @r_collision = true if collision_hor(bb.r,bb.t,bb.h)      
+    end
 
     #
-    # checking for collisions on the left and right sides of an aabb
-    #    
-    def vertical_map_collision      
-      #how many tiles can the top of the bounding box intersect
-      no_of_checks = ( bb.w / self.tile_map.tile_width).ceil       
-      no_of_checks +=1 if ((self.x-bb.w/2) % self.tile_map.tile_width) != 0
+    # checking for collisions on the top and bottom sides of an aabb
+    #
+    def collision_ver(x,y,width)
+      no_of_checks = ( width / self.tile_map.tile_width).ceil       
+      no_of_checks +=1 if (x % self.tile_map.tile_width) != 0
       
-      #check top of bb
-      first_tile_column = ((self.x-bb.w/2) / self.tile_map.tile_width).floor
-      first_tile_row = ((self.y-bb.h/2) / self.tile_map.tile_height).floor  
-          
-      no_of_checks.times do |i|
-        if self.tile_map.get_tile(first_tile_column+i,first_tile_row)
-          puts "top collision!" if self.tile_map.get_tile(first_tile_column+i,first_tile_row).solid          
-        end
-      end
-      
-      #check bottom of bb      
-      first_tile_row = (self.y+bb.h/2) / self.tile_map.tile_height
+      first_tile_column = (x / self.tile_map.tile_width).floor
+      first_tile_row = (y / self.tile_map.tile_height).floor  
       
       no_of_checks.times do |i|
         if self.tile_map.get_tile(first_tile_column+i,first_tile_row)
-          puts "bottom collision!" if self.tile_map.get_tile(first_tile_column+i,first_tile_row).solid          
+          return true if self.tile_map.get_tile(first_tile_column+i,first_tile_row).solid          
         end
       end
-      
+            
+      false
     end
     
     #
-    # checking for collisions on left and right side of aabb
+    # checking for collisions on the left and right sides of an aabb
     #
-    def horizontal_map_collision
-      #how many tiles can the left of the bounding box intersect
-      no_of_checks = ( bb.h / self.tile_map.tile_height).ceil       
-      no_of_checks +=1 if ((self.y-bb.h/2) % self.tile_map.tile_height) != 0
+    def collision_hor(x,y,height)
+      no_of_checks = ( height / self.tile_map.tile_height).ceil       
+      no_of_checks +=1 if (y % self.tile_map.tile_height) != 0
       
-      #check left of bb
-      first_tile_row = ((self.y-bb.h/2) / self.tile_map.tile_height).floor 
-      first_tile_column = ((self.x-bb.w/2) / self.tile_map.tile_width).floor
+      first_tile_row = (y / self.tile_map.tile_height).floor 
+      first_tile_column = (x / self.tile_map.tile_width).floor
            
       no_of_checks.times do |i|
         if self.tile_map.get_tile(first_tile_column,first_tile_row+i)
-          puts "left collision!" if self.tile_map.get_tile(first_tile_column,first_tile_row+i).solid          
+          return true if self.tile_map.get_tile(first_tile_column,first_tile_row+i).solid          
         end
       end
       
-      #check right of bb      
-      first_tile_column = ((self.x+bb.w/2) / self.tile_map.tile_width).floor
-      
-      no_of_checks.times do |i|
-        if self.tile_map.get_tile(first_tile_column,first_tile_row+i)
-          puts "right collision!" if self.tile_map.get_tile(first_tile_column,first_tile_row+i).solid            
-        end
-      end
-      
+      false
     end
     
   end
